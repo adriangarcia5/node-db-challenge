40b4ab1a6563554f2c0d0fe51e98b5f036ee4aab
Adrian-Garcia <agarc5@hotmail.com>
2019-10-11 10:46:13 -0500

    finished index and knex/added server to json file

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,16 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+      filename: './data/node-challenge.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+  }, 
 
   staging: {
     client: 'postgresql',
